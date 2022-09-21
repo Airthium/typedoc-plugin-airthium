0cde67308e6c5deae07542e55ef075dbbb285a57
Simon Garnotel <simon.garnotel@gmail.com>
2022-09-21 14:19:15 +0200

    --allow-empty

--- .github/release.sh
+++ .github/release.sh
@@ -15,36 +15,22 @@ check () {
     devLogs=$(git log dev -100 --oneline --pretty=format:"%H")
 
     hotfixCommit=$(git log hotfix -1 --oneline --pretty=format:"%H")
-    frontCommit=$(git log front -1 --oneline --pretty=format:"%H")
 
     hotfixOk=0
-    frontOk=0
     for devCommit in $devLogs
     do
         if [ "$devCommit" = "$hotfixCommit" ]
         then
             hotfixOk=1
         fi
-
-        if [ "$devCommit" = "$frontCommit" ]
-        then
-            frontOk=1
-        fi
     done
 
-    if [ $hotfixOk = 1 ] && [ $frontOk = 1 ]
+    if [ $hotfixOk = 1 ]
     then
         echo "Check passed, ready to merge"
     else
         echo "Error: branch mismatch"
-        if [ $hotfixOk = 0 ]
-        then
-            echo " - hotfix"
-        fi
-        if [ $frontOk = 0 ]
-        then
-            echo " - front"
-        fi
+        echo " - hotfix"
         exit 2
     fi
 }
@@ -59,11 +45,7 @@ merge () {
     git merge dev
     git push
 
-    git checkout front
-    git merge dev
-    git push
-
-    git checkout master
+    git checkout main
     git merge dev
     git push
 }
